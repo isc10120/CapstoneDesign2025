88ffda15f32abcc24fc40bd2c4eebb99d93b3db7
Ssomewhat <isc10120@gmail.com>
2026-04-13 17:51:37 +0900

    add: 문제 생성 관련 클래스 구조

---
+++ demo/src/main/kotlin/jamgaVOCA/demo/api/QuestionController.kt
@@ -0,0 +1,9 @@
+package jamgaVOCA.demo.api
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1")
+class QuestionController {
+}
---
+++ demo/src/main/kotlin/jamgaVOCA/demo/service/generateQuestion/QuestionGeneratorService.kt
@@ -0,0 +1,8 @@
+package jamgaVOCA.demo.service.generateQuestion
+
+import jamgaVOCA.demo.domain.word.WordRepository
+
+class QuestionGeneratorService(
+    private val wordRepository: WordRepository
+) {
+}
