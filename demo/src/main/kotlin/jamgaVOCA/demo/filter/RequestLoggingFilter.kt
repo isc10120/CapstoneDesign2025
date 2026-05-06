cde6deeed9a484d0261746a3b1e6f89f5276aec9
JeonHeewon <isc10120@gmail.com>
2026-05-06 21:30:28 +0900

    add: request/reponse 로깅

---
+++ demo/src/main/kotlin/jamgaVOCA/demo/filter/RequestLoggingFilter.kt
@@ -0,0 +1,24 @@
+package jamgaVOCA.demo.filter
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class RequestLoggingFilter : OncePerRequestFilter() {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        log.info("[REQUEST] ${request.method} ${request.requestURI} from ${request.remoteAddr}")
+        filterChain.doFilter(request, response)
+        log.info("[RESPONSE] ${response.status}")
+    }
+}
