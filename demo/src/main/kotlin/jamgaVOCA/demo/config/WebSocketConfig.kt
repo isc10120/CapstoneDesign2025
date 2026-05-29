94de55feba12c7454d71eef9335ce1e51e964b10
JeonHeewon <isc10120@gmail.com>
2026-05-30 00:07:55 +0900

    chore:  STOMP 연결/해제 시 로깅

--- demo/src/main/kotlin/jamgaVOCA/demo/config/WebSocketConfig.kt
+++ demo/src/main/kotlin/jamgaVOCA/demo/config/WebSocketConfig.kt
@@ -1,7 +1,15 @@
 package jamgaVOCA.demo.config
 
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.messaging.simp.stomp.StompCommand
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+import org.springframework.messaging.support.ChannelInterceptor
+import org.springframework.messaging.support.MessageHeaderAccessor
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
@@ -10,14 +18,33 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
-        registry.enableSimpleBroker("/topic")       // 구독 prefix
-        registry.setApplicationDestinationPrefixes("/app") // 발행 prefix
+        registry.enableSimpleBroker("/topic")
+        registry.setApplicationDestinationPrefixes("/app")
     }
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/ws")
             .setAllowedOriginPatterns("*")
-            .withSockJS()  // SockJS 폴백 지원
+            .withSockJS()
+    }
+
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        registration.interceptors(object : ChannelInterceptor {
+            override fun preSend(message: Message<*>, channel: MessageChannel): Message<*> {
+                val accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor::class.java)
+
+                when (accessor?.command) {
+                    StompCommand.CONNECT -> log.info("[STOMP] CONNECT")
+                    StompCommand.SUBSCRIBE -> log.info("[STOMP] SUBSCRIBE destination=${accessor.destination}")
+                    StompCommand.DISCONNECT -> log.info("[STOMP] DISCONNECT")
+                    else -> {}
+                }
+
+                return message
+            }
+        })
     }
 }
